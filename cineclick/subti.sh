6324e3e61343a3ec5628ad88e19f6c53054a1dbd
Zero-lalundonde <deja.vecu@hotmail.fr>
2015-09-07 17:49:14 -0300

    script convertir subtitulos .ass a .srt

--- cineclick/subti.sh
+++ cineclick/subti.sh
@@ -35,44 +35,42 @@
 
 #!/bin/bash
 
-touch /var/www/html/dibujitos/sub/tmp/prueba.srt
-touch /var/www/html/dibujitos/sub/tmp/prueba2.srt
-touch /var/www/html/dibujitos/sub/tmp/prueba3.srt
-touch /var/www/html/dibujitos/sub/tmp/prueba4.srt
-touch /var/www/html/dibujitos/sub/tmp/prueba5.srt
-touch /var/www/html/dibujitos/sub/tmp/prueba6.srt
+TMP=/var/www/dibujitos/sub/tmp
+SUB=/var/www/dibujitos/sub
 
-chmod 777 /var/www/html/dibujitos/sub/tmp/*
-chown apache:apache /var/www/html/dibujitos/sub/tmp/*
+touch $TMP/prueba.srt
+touch $TMP/prueba2.srt
+touch $TMP/prueba3.srt
+touch $TMP/prueba4.srt
+touch $TMP/prueba5.srt
+touch $TMP/prueba6.srt
 
+chmod 777 $TMP/*
+chown apache:apache $TMP/*
 
-sed -n -e '/^[Dialogue]/p' $1 >> /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
-sed -i 's/,/ /' /var/www/html/dibujitos/sub/tmp/prueba.srt
+sed -n -e '/^[Dialogue]/p' $1 >> $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
+sed -i 's/,/ /' $TMP/prueba.srt
 
-cut -d " " -f 1,3,4,10-50 /var/www/html/dibujitos/sub/tmp/prueba.srt >> /var/www/html/dibujitos/sub/tmp/prueba2.srt
-awk '{print "SALTO""SALTO" NR "SALTO" 0 $2 0, "-->", 0 $3 0 "SALTO";i=4; while (i<=NF){print $i ;i++}}' /var/www/html/dibujitos/sub/tmp/prueba2.srt >> /var/www/html/dibujitos/sub/tmp/prueba3.srt
-sed -n -e '1x;1!H;${x;s-\n- -gp}' /var/www/html/dibujitos/sub/tmp/prueba3.srt >> /var/www/html/dibujitos/sub/tmp/prueba4.srt
-sed -e 's/SALTO/\n/g' /var/www/html/dibujitos/sub/tmp/prueba4.srt >> /var/www/html/dibujitos/sub/tmp/prueba5.srt
-sed -e 's/\r//g' /var/www/html/dibujitos/sub/tmp/prueba5.srt  >> /var/www/html/dibujitos/sub/tmp/prueba6.srt
-sed -i 's/\\N/<br>/g' /var/www/html/dibujitos/sub/tmp/prueba6.srt
+cut -d " " -f 1,3,4,10-50 $TMP/prueba.srt >> $TMP/prueba2.srt
+awk '{print "SALTO""SALTO" NR "SALTO" 0 $2 0, "-->", 0 $3 0 "SALTO";i=4; while (i<=NF){print $i ;i++}}' $TMP/prueba2.srt >> $TMP/prueba3.srt
+sed -n -e '1x;1!H;${x;s-\n- -gp}' $TMP/prueba3.srt >> $TMP/prueba4.srt
+sed -e 's/SALTO/\n/g' $TMP/prueba4.srt >> $TMP/prueba5.srt
+sed -e 's/\r//g' $TMP/prueba5.srt  >> $TMP/prueba6.srt
+sed -i 's/\\N/<br>/g' $TMP/prueba6.srt
 nom=$(echo "$1" | rev | cut -d'.' -f2 | rev)
-cp /var/www/html/dibujitos/sub/tmp/prueba6.srt /var/www/html/dibujitos/sub/$2
+cp $TMP/prueba6.srt $SUB/$2
 
-
-rm -f /var/www/html/dibujitos/sub/tmp/prueba.srt
-rm -f /var/www/html/dibujitos/sub/tmp/prueba2.srt
-rm -f /var/www/html/dibujitos/sub/tmp/prueba3.srt
-rm -f /var/www/html/dibujitos/sub/tmp/prueba4.srt
-rm -f /var/www/html/dibujitos/sub/tmp/prueba5.srt
-rm -f /var/www/html/dibujitos/sub/tmp/prueba6.srt
-
-
-
+rm -f $TMP/prueba.srt
+rm -f $TMP/prueba2.srt
+rm -f $TMP/prueba3.srt
+rm -f $TMP/prueba4.srt
+rm -f $TMP/prueba5.srt
+rm -f $TMP/prueba6.srt
